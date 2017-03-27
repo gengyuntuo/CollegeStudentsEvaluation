4a6e8a4ecebd6e15868268388de4368ed7df4e11
Li Chun <gengyuntuo@163.com>
2017-03-27 17:39:21 +0800

    添加对Excel和Webparse项目的依赖

--- src/main/java/cn/xuemengzihe/sylu/ces/controller/web/SystemController.java
+++ src/main/java/cn/xuemengzihe/sylu/ces/controller/web/SystemController.java
@@ -1,13 +1,28 @@
 package cn.xuemengzihe.sylu.ces.controller.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * <h1>System Controller</h1>
  * <p>
+ * 系统设置
  * </p>
  * 
  * @author 李春
  * @time 2017年2月5日 上午11:11:57
  */
+@Controller
 public class SystemController {
+
+	/**
+	 * 打开系统设置界面
+	 * 
+	 * @return
+	 */
+	@RequestMapping("systemSetting")
+	public String systemSetting() {
+		return "/settting/setting";
+	}
 
 }
